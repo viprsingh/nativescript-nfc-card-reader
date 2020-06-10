829556caf19280b17ecd572c59b714ef6c0ac620
Singh <visingh@amazon.com>
2020-06-09 22:54:35 -0700

    Try calling NFC activity (partial) from demo angular app.

--- demo-angular/src/app/app.component.ts
+++ demo-angular/src/app/app.component.ts
@@ -1,6 +1,3 @@
-import { NfcCardReader } from 'nativescript-nfc-card-reader';
-console.log(new NfcCardReader().getCardNumber());
-console.log(new NfcCardReader().getExpiryDate());
 import { Component } from "@angular/core";
 
 @Component({
--- demo-angular/src/app/home/home.component.ts
+++ demo-angular/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import * as application from "tns-core-modules/application";
+declare var android: any;
+declare var com: any;
 
 @Component({
     selector: "Home",
@@ -12,5 +15,12 @@ export class HomeComponent implements OnInit {
 
     ngOnInit(): void {
         // Init your component properties here.
+        var intent = new android.content.Intent();
+        intent.setClassName("com.pro100svitlo.nfccardread", "com.pro100svitlo.nfccardread.MainActivity");
+        intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+        console.log(intent);
+        console.log(com.pro100svitlo.nfccardread.MainActivity.class);
+        let act = application.android.startActivity;
+        act.startActivity(intent);
     }
 }
