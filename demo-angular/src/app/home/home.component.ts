bc5c9322edd15ddef0c857a600dcd11c98761b4f
Singh <visingh@amazon.com>
2020-06-10 14:37:20 -0700

    Use class name to launch activity (partial).

--- demo-angular/src/app/home/home.component.ts
+++ demo-angular/src/app/home/home.component.ts
@@ -15,12 +15,12 @@ export class HomeComponent implements OnInit {
 
     ngOnInit(): void {
         // Init your component properties here.
-        var intent = new android.content.Intent();
-        intent.setClassName("com.pro100svitlo.nfccardread", "com.pro100svitlo.nfccardread.MainActivity");
-        intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
-        console.log(intent);
-        console.log(com.pro100svitlo.nfccardread.MainActivity.class);
+        let context = application.android.context;
+        console.log(context);
+        const mainActivity = com.pro100svitlo.nfccardread.MainActivity;
+        const nfcIntent = new android.content.Intent(context, mainActivity.class);
+        console.log(nfcIntent);
         let act = application.android.startActivity;
-        act.startActivity(intent);
+        act.startActivity(nfcIntent);
     }
 }
