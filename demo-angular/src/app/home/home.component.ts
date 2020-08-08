7b638aba60060046a8f3487174994e28892be38e
Rahul <rahultyagiji@gmail.com>
2020-08-08 14:36:15 +1000

    converting card read to async function

--- demo-angular/src/app/home/home.component.ts
+++ demo-angular/src/app/home/home.component.ts
@@ -7,6 +7,14 @@ import { NfcCardReader } from "../../../../src";
 })
 export class HomeComponent implements OnInit {
 
+
+    private cardNumber: string="";
+    private cardExpiry:string="";
+    public itemDesc:string=""
+    public itemValue:number=0;
+    public addItem:boolean=false;
+    itemObject:{"description":string,"value":number}[]=[];
+    totalAmount:number=0;
     constructor() {
         // Use the component constructor to inject providers.
     }
@@ -15,9 +23,32 @@ export class HomeComponent implements OnInit {
 
     }
     clickCard(){
+        this.cardExpiry=""
+        this.cardNumber=""
         let nfcCardReader = new NfcCardReader();
-        nfcCardReader.readCard();
-        console.log(nfcCardReader.getCardNumber());
-        console.log(nfcCardReader.getExpiryDate());
+        nfcCardReader.readCard()
+            .then(()=>{
+                    this.cardNumber = nfcCardReader.getCardNumber()
+                    this.cardExpiry = (new Date(nfcCardReader.getExpiryDate()).getMonth() + 1).toString();
+                    nfcCardReader.closeListener();
+            })
+
     }
+
+    onAddItem(){
+
+        this.addItem=true;
+    }
+
+    addItemToObject(item){
+        console.log(item, this.itemDesc)
+    }
+
+    totalValue(){
+        this.totalAmount = 0;
+        this.itemObject.forEach((x)=>{
+            this.totalAmount += x.value
+        })
+    }
+
 }
